15b207466428202905c9e3789cf3b4f3aa389d71
Lyall <me@lyall.lol>
2024-09-09 16:24:07 +0100

    fix objectDefaults

--- index.js
+++ index.js
@@ -447,12 +447,12 @@ function stringifyCookies(cookies) {
  * @returns {object} Merged objects
  */
 function objectDefaults(obj, def) {
-    if (obj === null || typeof obj !== "object") return def;
+    if (!obj instanceof Object) return def;
     
     return (function checkEntries(object = obj, defaultObj = def) {
         Object.entries(defaultObj).forEach(([key, value]) => {
             if (object[key] === undefined) object[key] = value;
-            else if (value !== null && typeof value === "object" && typeof object[key] === "object") checkEntries(object[key], value);
+            else if (value instanceof Object && object[key] instanceof Object) checkEntries(object[key], value);
         });
         return object;
     })();
